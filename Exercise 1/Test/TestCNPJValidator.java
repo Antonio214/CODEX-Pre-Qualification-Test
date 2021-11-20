bec9b9dbf8b046ee6b16c3b6ac91ef1ed6049ad1
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-11-20 18:56:06 -0300

    first test: failing test

---
+++ Exercise 1/Test/TestCNPJValidator.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class TestCNPJValidator {
+    String validCNPJ = "43517047000130";
+    CNPJValidator validator = new CNPJValidator();
+
+    @Test
+    public void shouldInformValidNumberCNPJ() {
+        System.out.println("This should inform that a number format CNPJ is valid!");
+        Assertions.assertEquals(validator.check(validCNPJ), "CNPJ informado é valido",
+                "App is not recognizing a valid CPNJ as valid");
+    }
+}
