eb04e252bcea33b0e24814358c0bc9f2e3a0d0c5
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-11-20 20:09:46 -0300

    added all messages to constants

--- Exercise 1/src/constants/Messages.java
+++ Exercise 1/src/constants/Messages.java
@@ -3,4 +3,6 @@ package constants;
 public class Messages {
     public static final String VALID_CNPJ = "CNPJ informado é valido";
     public static final String INVALID_DIGIT = "CNPJ informado não é valido, erro no dígito verificador";
+    public static final String INVALID_AMOUNT_DIGIT = "CNPJ informado não possui a quantidade correta de dígitos";
+    public static final String INVALID_MASK = "CNPJ não possui máscara válida";
 }
