8f54a470836f68f7e3febafdde275bf80847e0d5
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-11-20 19:58:35 -0300

    improved first and second test cases: added more cases to confirm logic

--- Exercise 1/Test/TestCNPJValidator.java
+++ Exercise 1/Test/TestCNPJValidator.java
@@ -5,21 +5,28 @@ import constants.Messages;
 import modules.CNPJValidator;
 
 public class TestCNPJValidator {
-    String validCNPJ = "43517047000130";
-    String invalidCNPJ = "11111111111111";
+    String[] validCNPJs = { "43517047000130", "82867857000178", "09918723000149", "32601289000190", "20499660000176" };
+    String[] invalidCNPJs = { "11111111111111", "22222222222222", "33333333333333", "44444444444444",
+            "55555555555555" };
     CNPJValidator validator = new CNPJValidator();
 
     @Test
     public void shouldInformValidNumberCNPJ() {
         System.out.println("This should inform that a number format CNPJ is valid!");
-        Assertions.assertEquals(validator.check(validCNPJ), Messages.VALID_CNPJ,
-                "App is not recognizing a valid CPNJ as valid");
+
+        for (String validCNPJ : validCNPJs) {
+            Assertions.assertEquals(validator.check(validCNPJ), Messages.VALID_CNPJ,
+                    "App is not recognizing a valid CPNJ as valid");
+        }
     }
 
     @Test
     public void shouldInformNotValidNumberCNPJ() {
         System.out.println("This should inform that a number format CNPJ is not valid!");
-        Assertions.assertEquals(validator.check(invalidCNPJ), Messages.INVALID_DIGIT,
-                "App is not recognizing a CNPJ as invalid");
+
+        for (String invalidCNPJ : invalidCNPJs) {
+            Assertions.assertEquals(validator.check(invalidCNPJ), Messages.INVALID_DIGIT,
+                    "App is not recognizing a CNPJ as invalid");
+        }
     }
 }
