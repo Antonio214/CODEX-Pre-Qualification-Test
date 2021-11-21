de349b855df580124a49534fc28634c937d882c1
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-11-21 15:47:35 -0300

    first test: failing test

---
+++ Exercise 2/src/constants/Messages.java
@@ -0,0 +1,6 @@
+package constants;
+
+public class Messages {
+    public static final String INTERSECT_MESSAGE = "Intersectam";
+    public static final String DONT_INTERSECT_MESSAGE = "Não Intersectam";
+}
---
+++ Exercise 2/test/TestRectIntersect.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import constants.Messages;
+
+public class TestRectIntersect {
+    @Test
+    public void shouldInformTwoRectsDontIntersect() {
+        System.out.println("This should inform that rect (0,0;1,1) and rect (2,2;3,3) dont intersect");
+        
+        RectangleModel rectA = new RectangleModel("(0,0;1,1)");
+        RectangleModel rectB = new RectangleModel("(2,2;3,3)");
+
+        IntersectionVerifier verifier = new IntersectionVerifier();
+        String resultMessage = verifier.verify(rectA, rectB); 
+
+        Assertions.assertEquals(Messages.DONT_INTERSECT_MESSAGE, resultMessage);    
+}
