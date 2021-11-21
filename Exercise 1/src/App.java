4abf8739f26c8c4e675ee13257a9de70e8f7aa03
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-11-21 13:15:06 -0300

    added cli and final comments to Todo

--- Exercise 1/src/App.java
+++ Exercise 1/src/App.java
@@ -1,9 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 import Models.CNPJModel;
 import modules.CNPJValidator;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        CNPJModel cnpjModel = new CNPJModel("43517047000130");
+        System.out.println("Insira o CNPJ para ser validado: ");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String cnpjFromInput = reader.readLine();
+
+        CNPJModel cnpjModel = new CNPJModel(cnpjFromInput);
         CNPJValidator validator = new CNPJValidator();
 
         System.out.println(validator.check(cnpjModel));
