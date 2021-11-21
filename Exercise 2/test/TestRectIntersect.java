ff05a47481d4763d2e261bf8802e0feaddd5af73
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-11-21 18:15:14 -0300

    third test passed and forth failing test

--- Exercise 2/test/TestRectIntersect.java
+++ Exercise 2/test/TestRectIntersect.java
@@ -8,6 +8,8 @@ import modules.IntersectionVerifier;
 public class TestRectIntersect {
     RectangleModel rectA = new RectangleModel("(0,0;1,1)");
     RectangleModel rectB = new RectangleModel("(2,2;3,3)");
+    RectangleModel rectC = new RectangleModel("(1,1;3,3)");
+    RectangleModel rectD = new RectangleModel("(0,0;2,2)");
     IntersectionVerifier verifier = new IntersectionVerifier();
 
     @Test
@@ -22,6 +24,22 @@ public class TestRectIntersect {
     public void shouldInformSameRectIntersect() {
         System.out.println("This should inform that rect (0,0;1,1) and rect (0,0;1,1) intersect");
         String resultMessage = verifier.verify(rectA, rectA);
+
+        Assertions.assertEquals(Messages.INTERSECT_MESSAGE, resultMessage);
+    }
+
+    @Test
+    public void shouldInformSameEdgeRectIntersect() {
+        System.out.println("This should inform that rect (0,0;1,1) and rect (1,1;3,3) intersect");
+        String resultMessage = verifier.verify(rectA, rectC);
+
+        Assertions.assertEquals(Messages.INTERSECT_MESSAGE, resultMessage);
+    }
+
+    @Test
+    public void shouldInformRectIntersect() {
+        System.out.println("This should inform that rect (0,0;2,2) and rect (1,1;3,3) intersect");
+        String resultMessage = verifier.verify(rectC, rectD);
 
         Assertions.assertEquals(Messages.INTERSECT_MESSAGE, resultMessage);
     }
