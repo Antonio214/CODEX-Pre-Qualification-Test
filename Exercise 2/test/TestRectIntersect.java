c87b9b18233455836d2a5280f1c79b514572cee8
Antonio Junior ra165677 <fXXXXXX@dac.unicamp.br>
2021-11-21 15:53:52 -0300

    second test: failing test

--- Exercise 2/test/TestRectIntersect.java
+++ Exercise 2/test/TestRectIntersect.java
@@ -6,16 +6,23 @@ import models.RectangleModel;
 import modules.IntersectionVerifier;
 
 public class TestRectIntersect {
+    RectangleModel rectA = new RectangleModel("(0,0;1,1)");
+    RectangleModel rectB = new RectangleModel("(2,2;3,3)");
+    IntersectionVerifier verifier = new IntersectionVerifier();
+
     @Test
     public void shouldInformTwoRectsDontIntersect() {
         System.out.println("This should inform that rect (0,0;1,1) and rect (2,2;3,3) dont intersect");
-
-        RectangleModel rectA = new RectangleModel("(0,0;1,1)");
-        RectangleModel rectB = new RectangleModel("(2,2;3,3)");
-
-        IntersectionVerifier verifier = new IntersectionVerifier();
         String resultMessage = verifier.verify(rectA, rectB);
 
         Assertions.assertEquals(Messages.DONT_INTERSECT_MESSAGE, resultMessage);
+    }
+
+    @Test
+    public void shouldInformSameRectIntersect() {
+        System.out.println("This should inform that rect (0,0;1,1) and rect (0,0;1,1) intersect");
+        String resultMessage = verifier.verify(rectA, rectB);
+
+        Assertions.assertEquals(Messages.INTERSECT_MESSAGE, resultMessage);
     }
 }
